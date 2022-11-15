28157bb0bbfa27bce81fe53ce5c360404e27557a
Jonathan Derek McArthur <jondmcarthur@gmail.com>
2022-11-14 21:45:15 -0600

    Fixed seed file

--- pokemon-server/db/seeds.rb
+++ pokemon-server/db/seeds.rb
@@ -13,17 +13,21 @@ end
     pokemon_ids.each do |pokemon_id|
         response = RestClient.get "https://pokeapi.co/api/v2/pokemon/#{pokemon_id}/"
         pokemon_id_hash = JSON.parse(response)
+        type_array = pokemon_id_hash["types"]
+        case type_array.length
+        when 2 
+            first_type = pokemon_id_hash["types"][0]["type"]["name"]
+            second_type = pokemon_id_hash["types"][1]["type"]["name"]
+        else 
+            first_type = pokemon_id_hash["types"][0]["type"]["name"]
+            second_type = "N/A"
+        end
         
         Pokemon.create(
             pokemon_ID: pokemon_id,
             name: pokemon_id_hash["name"],
-            #  if (pokemon_id_hash["types"].length = 2)
-            #      first_type: pokemon_id_hash["types"][0]["type"]["name"],
-            #      second_type: pokemon_id_hash["types"][1]["type"]["name"],
-            # else
-            first_type: pokemon_id_hash["types"][0]["type"]["name"],
-            second_type: "N/A",
-            # end
+            first_type: first_type,
+            second_type: second_type,
             sprite: pokemon_id_hash["sprites"]["other"]["official-artwork"]["front_default"],
             trainer_id: i + 1
             )
